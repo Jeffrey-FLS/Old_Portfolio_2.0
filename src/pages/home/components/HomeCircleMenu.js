62607c3121994a96f34a828574ed905106f240ae
Jeffrey Paulino <jp7kode@gmail.com>
2019-12-06 20:25:25 -0500

    Working on circle sides position

--- src/pages/home/components/HomeCircleMenu.js
+++ src/pages/home/components/HomeCircleMenu.js
@@ -1,11 +1,20 @@
 
 import React, {Component} from 'react';
 import './HomeCircleMenu.scss';
+import image from "../../../assets/ring-divided.png";
 
 class HomeCircleMenu extends Component {
     render() {
         return (
             <div className="homepage_-_circle-menu">
+
+
+                <div className="homepage_-_circle-menu_--_side-projects circle-side"></div>
+                <div className="homepage_-_circle-menu_--_side-blogs circle-side"></div>
+                <div className="homepage_-_circle-menu_--_side-about circle-side"></div>
+
+                <img src={image} alt="" className="tempImage"/>
+
                 {/*<div className="tempbody">*/}
                     <div className="tempcircle"></div>
                 {/*</div>*/}
